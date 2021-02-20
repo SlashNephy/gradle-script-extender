efaeea195c671ed4fbbf2a78a929f00ab2207ea4
Nep <spica@starry.blue>
2021-02-20 19:37:57 +0900

    receiver of env should be Any?

--- src/main/kotlin/blue/starry/scriptextender/env.kt
+++ src/main/kotlin/blue/starry/scriptextender/env.kt
@@ -2,18 +2,13 @@ package blue.starry.scriptextender
 
 import org.gradle.api.Project
 import kotlin.properties.ReadOnlyProperty
-import kotlin.reflect.KProperty
 
 fun Project.env(name: String): EnvReference {
     return EnvReference(name)
 }
 
-val Project.env: ReadOnlyProperty<Project, EnvReference>
-    get() = object: ReadOnlyProperty<Project, EnvReference> {
-        override fun getValue(thisRef: Project, property: KProperty<*>): EnvReference {
-            return env(property.name)
-        }
-    }
+val Project.env: ReadOnlyProperty<Any?, EnvReference>
+    get() = ReadOnlyProperty { _, property -> env(property.name) }
 
 data class EnvReference(private val name: String) {
     val value: String
